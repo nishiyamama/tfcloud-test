ef039055097f280d7c7e5a9c9a71b31fa33cca29
root <root@ip-10-0-1-12.ap-northeast-1.compute.internal>
2019-10-10 09:57:44 +0900

    Editting desuyo

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags {
-    Name = "tfcloud-desu-${terraform.env}-vpc"
+    Name = "tfcloud-desuyo-${terraform.env}-vpc"
     Environment = "${terraform.env}"
   }
 }
