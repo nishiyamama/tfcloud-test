156e70a153a59f661017b55328c93c5a61763de3
root <root@ip-10-0-1-12.ap-northeast-1.compute.internal>
2019-10-10 09:36:11 +0900

    Edit Files

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "region" {  default = "ap-northeast-1"}
-variable "vpc_cidr" {  default = "10.0.0.0/16"}
+variable "vpc_cidr" {  default = "10.1.0.0/16"}
 variable "ssh_key_name" {  default = "nshimayangform"}
